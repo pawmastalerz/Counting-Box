6deef8bb64a322dcde4aea5b7fcfe18ddac9104f
pawmastalerz <paw.mastalerz@gmail.com>
2017-11-10 23:03:27 +0100

    Minor code changes.

--- code/code.ino
+++ code/code.ino
@@ -85,32 +85,3 @@ unsigned long longToPrint(int x)
       break;
   }
 }
-
-/*bool getTime(const char *str)
-{
-  int Hour, Min, Sec;
-
-  if (sscanf(str, "%d:%d:%d", &Hour, &Min, &Sec) != 3) return false;
-  tm.Hour = Hour;
-  tm.Minute = Min;
-  tm.Second = Sec;
-  return true;
-}
-
-bool getDate(const char *str)
-{
-  char Month[12];
-  int Day, Year;
-  uint8_t monthIndex;
-
-  if (sscanf(str, "%s %d %d", Month, &Day, &Year) != 3) return false;
-  for (monthIndex = 0; monthIndex < 12; monthIndex++)
-  {
-    if (strcmp(Month, monthName[monthIndex]) == 0) break;
-  }
-  if (monthIndex >= 12) return false;
-  tm.Day = Day;
-  tm.Month = monthIndex + 1;
-  tm.Year = CalendarYrToTm(Year);
-  return true;
-}*/
